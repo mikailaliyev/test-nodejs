0d524334edcdb350e18ebf9bed793f112daa335a
mikailaliyev <mikail.aliyev@gmail.com>
2022-04-23 12:58:15 +0400

    added favicon route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { readFileSync, read } = require("fs");
 const homePage = readFileSync("./public/index.html");
 const errorPage = readFileSync("./public/error.html", "utf8");
 const errorPageStyles = readFileSync("./public/styles.css");
+const favicon = readFileSync("./public/favicon.png");
 
 http
   .createServer((req, res) => {
@@ -17,6 +18,10 @@ http
       res.end(`<h1 style='text-align: center; color: blue'>
           About page
           </h1>`);
+    } else if (url === "/favicon.png") {
+      res.writeHead(200, { "content-type": "image/x-icon" });
+      res.write(favicon);
+      res.end();
     } else if (url === "/styles.css") {
       res.writeHead(200, { "content-type": "text/css" });
       res.write(errorPageStyles);
